599fcd90d32d62c79bec853fde63c187975d0992
Mariusz Wiśniewski <mariuszwisniewski88@gmail.com>
2026-01-18 23:01:44 +0100

    Added my linting rules and applying them
    Additionally included .env file with VITE variables.

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
-])
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{js,jsx}'],
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite
+        ],
+        plugins: {
+            '@stylistic': stylistic
+        },
+        languageOptions: {
+            ecmaVersion:   2020,
+            globals:       globals.browser,
+            parserOptions: {
+                ecmaVersion:  'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType:   'module'
+            }
+        },
+        rules: {
+            'no-unused-vars':          ['error', { varsIgnorePattern: '^[A-Z_]' }],
+            '@stylistic/indent':       ['error', 4],
+            '@stylistic/semi':         ['error', 'always'],
+            '@stylistic/comma-dangle': ['error', 'never']
+        }
+    }
+]);
